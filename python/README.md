bf57828bc31dc858d6e7b17383b986bd4dfecb15
Anton Kochkov <anton.kochkov@gmail.com>
2020-12-09 17:38:08 +0800

    Rzpipe Python fixes

--- python/README.md
+++ python/README.md
@@ -1,17 +1,17 @@
 # rzpipe for Python
 
-Interact with rizin using the #!pipe command or in standalone scripts
-that communicate with local or remote r2 via pipe, tcp or http.
+Interact with rizin using the `#!pipe` command or in standalone scripts
+that communicate with local or remote rizin via pipe, tcp or http.
 
 ## Installation
 
-```
+```sh
 $ pip install rzpipe
 ```
 
 or
 
-```
+```sh
 $ pip3 install rzpipe
 ```
 
@@ -23,7 +23,7 @@ import rzpipe
 pipe = rzpipe.open("/bin/ls")
 pipe.cmd('aa')
 print(pipe.cmd("afl"))
-print(pipe.cmdj("aflj"))            # evaluates JSONs and returns an object
+print(pipe.cmdj("aflj"))            # evaluates JSON and returns an object
 print(pipe.cmdj("ij").core.format)  # shows file format
 pipe.quit()
 ```
--- python/setup.py
+++ python/setup.py
@@ -2,17 +2,29 @@
 from distutils.core import setup
 import rzpipe
 
-# with open('README.rst') as readme_file:
-#    readme = readme_file.read()
+def readme():
+    with open('README.md') as readme_file:
+        return readme_file.read()
 
 setup(
     name="rzpipe",
     version=rzpipe.version(),
-    license="MIT",
     description="Pipe interface for rizin",
+    long_description=readme(),
+    long_description_content_type='text/markdown',
     author="pancake",
-    author_email="pancake@nopcode.org",
-    url="https://rada.re",
+    author_email="info@rizin.re",
+    url="https://rizin.re",
+    license="MIT",
     package_dir={"rzpipe": "rzpipe"},
     packages=["rzpipe"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Topic :: Security",
+        "Topic :: Software Development :: Assemblers"
+        "Topic :: Software Development :: Debuggers"
+        "Topic :: Software Development :: Disassemblers"
+    ],
 )
