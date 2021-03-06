fda6c78135c3298e992d95a01758758496c1336b
4thel00z <4thel00z@gmail.com>
2021-03-06 11:14:36 +0100

    Update Python style and add pytest
    * Use shutil to figure out where ls lives
    * Add contextlib shim for OpenBase
    * Improve OpenBase
    * Make shutil portable across python2/3
    * Use print to print to sys.stderr
    * Renames for easier readibility
    * Improve indentation of help strings
    * Improve namings and fix default param list issue
    * Simplify comparison in native.py
    * Add requirements-dev.txt
    * Add format target which formats via black
    * Reformat using black
    * Improve the native module
    * Remove unnecessary usage of eval
    * Make pass through of lib functional
    * Improve import logic
    * Add context manager for convenience
    * Add a test for the native module
    * Add pytest make target
    * Use asyncio >= 3.6 style, improve overall quality
    * Add open_base_test
    
    Signed-off-by: 4thel00z <4thel00z@gmail.com>

--- python/rzpipe/native.py
+++ python/rzpipe/native.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys
+import contextlib
+import ctypes
 from ctypes import Structure, addressof, c_char_p, c_void_p
 from ctypes.util import find_library
 
+lib_name = find_library("rz_core")
+
+if not lib_name:
+    raise ImportError("No native rz_core library")
+
 try:
     from ctypes import CDLL
-except:
+
+    lib = CDLL(lib_name)
+except ImportError:
     pass
 
 try:
     from ctypes import WinDLL
-except:
-    pass
 
-lib_name = find_library("rz_core")
-if lib_name == None:
-    raise ImportError("No native rz_core library")
-
-if sys.platform.startswith("win"):
     lib = WinDLL(lib_name)
-else:
-    lib = CDLL(lib_name)
+except ImportError:
+    pass
 
 
 class AddressHolder(object):
@@ -36,26 +37,28 @@ class AddressHolder(object):
 
 
 class WrappedRMethod(object):
-    def __init__(self, cname, args, ret):
+    def __init__(self, cname, args, ret, lib=lib, ):
         self.cname = cname
         self.args = args
         self.ret = ret
         self.args_set = False
         self.method = getattr(lib, cname)
 
-    def __call__(self, *a):
+    def __call__(self, *args):
         if not self.args_set:
             if self.args:
-                self.method.argtypes = [eval(x.strip()) for x in self.args.split(",")]
-            self.method.restype = eval(self.ret) if self.ret else None
+                self.method.argtypes = [
+                    getattr(ctypes, x.strip()) for x in self.args.split(",")
+                ]
+            self.method.restype = getattr(ctypes, self.ret) if self.ret else None
             self.args_set = True
-        a = list(a)
-        for i, argt in enumerate(self.method.argtypes):
-            if argt is c_char_p:
-                a[i] = a[i].encode()
+        args = list(args)
+        for i, arg_type in enumerate(self.method.argtypes):
+            if arg_type is c_char_p:
+                args[i] = args[i].encode()
         if self.method.restype is c_char_p:
-            return self.method(*a).decode()
-        return self.method(*a)
+            return self.method(*args).decode()
+        return self.method(*args)
 
 
 class WrappedApiMethod(object):
@@ -71,20 +74,21 @@ class WrappedApiMethod(object):
             if self.ret2 == "c_char_p":
                 return result
             else:
-                result = eval(self.ret2)(result)
+                result = getattr(ctypes, self.ret2)(result)
         if self.last:
             return getattr(result, self.last)
         return result
 
     def __get__(self, obj, type_):
-        self._o = obj._o
+        if type_ is RzCore:
+            self._o = obj._o
         return self
 
 
-def register(cname, args, ret):
+def register(cname, args, ret, lib=lib):
     ret2 = last = None
     if ret:
-        if ret[0] >= "A" and ret[0] <= "Z":
+        if "A" <= ret[0] <= "Z":
             x = ret.find("<")
             if x != -1:
                 ret = ret[0:x]
@@ -94,13 +98,13 @@ def register(cname, args, ret):
             last = "value"
             ret2 = ret
 
-    method = WrappedRMethod(cname, args, ret)
+    method = WrappedRMethod(cname, args, ret, lib=lib,)
     wrapped_method = WrappedApiMethod(method, ret2, last)
     return wrapped_method, method
 
 
 class RzCore(Structure):  # 1
-    def __init__(self):
+    def __init__(self, lib=lib):
         Structure.__init__(self)
         rz_core_new = lib.rz_core_new
         rz_core_new.restype = c_void_p
@@ -109,11 +113,32 @@ class RzCore(Structure):  # 1
     _o = AddressHolder()
 
     cmd_str, rz_core_cmd_str = register(
-        "rz_core_cmd_str", "c_void_p, c_char_p", "c_char_p"
+        "rz_core_cmd_str",
+        "c_void_p, c_char_p",
+        "c_char_p",
     )
-    free, rz_core_free = register("rz_core_free", "c_void_p", "c_void_p")
+
+    free, rz_core_free = register(
+        "rz_core_free",
+        "c_void_p",
+        "c_void_p",
+    )
 
 
+@contextlib.contextmanager
+def core():
+    c = RzCore()
+    yield c
+    c.free()
+
+
+__all__ = [
+    core.__name__,
+    register.__name__,
+    RzCore.__name__,
+    WrappedRMethod.__name__,
+    WrappedApiMethod.__name__,
+]
 #  c = RzCore()
 #  c.cmd_str("o /bin/ls")
 #  print(c)
