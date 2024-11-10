c06fe77fd223dd13ef91f6474f80203b2c3936aa
Khairul Azhar Kasmiran <kazarmy@gmail.com>
2024-11-10 13:59:58 +0800

    Remove rz-agent use from open_base_test.py test

--- python/tests/open_base_test.py
+++ python/tests/open_base_test.py
@@ -1,34 +1,9 @@
-import contextlib
-import os
 import sys
 from shutil import which
 
 import pytest
 
 from rzpipe import open_base
-
-
-def kill(process_name):
-    os.system(f"pkill {process_name}") == 0 or \
-    os.system(f"killall {process_name}") == 0 or \
-    os.system(f"taskkill /f /im  {process_name}") == 0 or \
-    os.system(f"taskkill /f /im  {process_name}.exe") == 0
-
-
-def setup(port: int = 9080):
-    kill("rz-agent")
-    os.system(f"rz-agent -p{port} -d")
-
-
-def tear_down():
-    kill("rz-agent")
-
-
-@contextlib.contextmanager
-def linux_test():
-    setup()
-    yield
-    tear_down()
 
 
 def test_constructor_win():
@@ -46,6 +21,5 @@ def test_constructor_linux():
     """
     if sys.platform.startswith("win"):
         pytest.skip("skipping linux-only tests")
-    with linux_test():
-        with open_base.OpenBase("http://localhost:9080") as base:
-            base.cmd(f"o {which('/bin/ls')}")
+    with open_base.OpenBase(which('ls')) as base:
+        base.cmd("s entry0; pd 20")
